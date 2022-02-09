178c87ebf8ce2155fcf8a9610f76bef74f926bc4
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-09 20:49:19 +0000

    setup basic api to list synthetics (#6)

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/easyCZ/qfy/internal/api"
 	"github.com/easyCZ/qfy/internal/db"
 	"github.com/go-chi/chi/v5"
 )
@@ -31,7 +33,25 @@ func main() {
 		log.Fatalf("Failed to create db record: %v", err)
 	}
 
+	repo := db.NewSyntheticsRepository(database)
+	service := api.NewSyntheticsService(repo)
+
 	r := chi.NewRouter()
+	r.Get("/synthetics", func(rw http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		syns, err := service.List(ctx)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+		}
+
+		enc := json.NewEncoder(rw)
+		if err := enc.Encode(syns); err != nil {
+			log.Println("Failed to serialize synthetics to response")
+			return
+		}
+
+		return
+	})
 
 	log.Printf("Starting server on http://localhost:3000")
 	http.ListenAndServe(":3000", r)
