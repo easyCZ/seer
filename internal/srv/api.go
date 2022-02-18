fca97a2f6e67813f57958fc8b7ff290189bc5e40
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-18 09:23:59 +0000

    Implement synthetics service in grpc

--- internal/srv/api.go
+++ internal/srv/api.go
@@ -1,14 +1,14 @@
 package srv
 
 import (
-	agentv1 "github.com/easyCZ/qfy/gen/v1"
-	"github.com/easyCZ/qfy/internal/api"
-	"github.com/easyCZ/qfy/internal/db"
-	"github.com/go-chi/chi/v5"
-	"google.golang.org/grpc"
+	"fmt"
 	"log"
 	"net"
-	"net/http"
+
+	apiv1 "github.com/easyCZ/qfy/gen/v1"
+	"github.com/easyCZ/qfy/internal/db"
+	"google.golang.org/grpc"
+	"gorm.io/gorm"
 )
 
 type CPConfig struct {
@@ -16,67 +16,45 @@ type CPConfig struct {
 }
 
 func ListenAndServeControlPlane(c CPConfig) error {
-	database, err := db.New(c.DB)
+	database, err := setupDB(c.DB)
 	if err != nil {
 		log.Fatalf("Failed to setup db: %v", err)
 	}
 
-	if err := db.Migrate(database); err != nil {
-		log.Fatalf("Failed to migrate DB: %v", err)
-	}
-
-	repo := db.NewSyntheticsRepository(database)
-	service := api.NewSyntheticsService(repo)
-
+	syntheticsRepo := db.NewSyntheticsRepository(database)
 	agentsRepo := db.NewAgentsRepository(database)
 
-	//if _, err := repo.Create(context.Background(), &db.Synthetic{
-	//	Name: "initial-synthetic",
-	//	Spec: db.SyntheticSpec{
-	//		Variables: []*db.Variable{
-	//			{
-	//				Name:  "TEST",
-	//				Value: "foo",
-	//			},
-	//		},
-	//		Steps: []*db.StepSpec{
-	//			{
-	//				URL: "https://jsonplaceholder.typicode.com/posts",
-	//				Headers: map[string]string{
-	//					"Content-Type": "application/json",
-	//				},
-	//			},
-	//		},
-	//	},
-	//}); err != nil {
-	//	log.Fatalf("Failed to create synthetic: %v", err)
-	//}
+	syntheticsSvc := &SyntheticsService{repo: syntheticsRepo}
+	agentsSvc := &AgentService{repo: agentsRepo}
 
-	listener, err := net.Listen("tcp", "localhost:3001")
+	grpcServer := grpc.NewServer()
+
+	apiv1.RegisterAgentServiceServer(grpcServer, agentsSvc)
+	apiv1.RegisterSyntheticsServiceServer(grpcServer, syntheticsSvc)
+
+	listener, err := net.Listen("tcp", "localhost:3000")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 3001, %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
-	agentv1.RegisterAgentServiceServer(grpcServer, &AgentService{
-		repo: agentsRepo,
-	})
-
-	go func() {
-		log.Printf("Starting gRPC server on localhost:3001")
-		if err := grpcServer.Serve(listener); err != nil {
-			log.Fatalf("gRPC server failed to start: %v", err)
-		}
-	}()
-
-	r := chi.NewRouter()
-	api.AddRoutes(r, service)
-
-	log.Printf("Starting server on http://localhost:3000")
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatalf("Failed to start HTTP server: %v", err)
+	log.Printf("Starting gRPC server on localhost:3000")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("gRPC server failed to start: %v", err)
 	}
 
-	log.Printf("Finished serving API.")
+	log.Printf("Finished serving gRPC API.")
 	return nil
+}
+
+func setupDB(params db.ConnectionParams) (*gorm.DB, error) {
+	database, err := db.New(params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup db: %v", err)
+	}
+
+	if err := db.Migrate(database); err != nil {
+		return nil, fmt.Errorf("failed to migrate DB: %v", err)
+	}
+
+	return database, nil
 }
--- internal/srv/synthetics_service.go
+++ internal/srv/synthetics_service.go
@@ -1,6 +1,67 @@
 package srv
 
-type SyntheticsService struct {
+import (
+	"fmt"
+	"net/http"
 
+	apiv1 "github.com/easyCZ/qfy/gen/v1"
+	"github.com/easyCZ/qfy/internal/db"
+)
+
+type SyntheticsService struct {
+	repo *db.SyntheticsRepository
 }
 
+func (s *SyntheticsService) ListSynthetics(req *apiv1.ListSyntheticsRequest, stream apiv1.SyntheticsService_ListSyntheticsServer) error {
+	synthetics, err := s.repo.List(stream.Context())
+	if err != nil {
+		return fmt.Errorf("failed to list synthetics: %w", err)
+	}
+
+	for _, s := range synthetics {
+		if err := stream.Send(dbSyntheticToV1(s)); err != nil {
+			return fmt.Errorf("failed to send synthetic to client: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func dbSyntheticToV1(s *db.Synthetic) *apiv1.Synthetic {
+	return &apiv1.Synthetic{
+		Id:   fmt.Sprintf("%d", s.ID),
+		Name: s.Name,
+		Spec: dbSyntheticSpecToV1(&s.Spec),
+	}
+}
+
+func dbSyntheticSpecToV1(spec *db.SyntheticSpec) *apiv1.SyntheticSpec {
+	var variables []*apiv1.Variable
+	for _, v := range spec.Variables {
+		variables = append(variables, &apiv1.Variable{
+			Name:  v.Name,
+			Value: v.Value,
+		})
+	}
+
+	var steps []*apiv1.Step
+	for _, s := range spec.Steps {
+		steps = append(steps, dbStepToV1(s))
+	}
+
+	return &apiv1.SyntheticSpec{
+		Variables: variables,
+		Steps:     steps,
+	}
+}
+
+func dbStepToV1(step *db.StepSpec) *apiv1.Step {
+	return &apiv1.Step{
+		Name: "TODO",
+		Spec: &apiv1.StepSpec{
+			Url:    step.URL,
+			Method: http.MethodGet,
+			Body:   *step.Body,
+		},
+	}
+}
