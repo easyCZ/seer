c4ee53b14f1ddc195c412379613587a06d565115
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-22 09:24:24 +0000

    fix

--- internal/srv/run_service.go
+++ internal/srv/run_service.go
@@ -1,15 +1,17 @@
 package srv
 
 import (
+	"context"
+
 	apiv1 "github.com/easyCZ/qfy/gen/v1"
 )
 
 type RunService struct {
 }
 
-func (s *RunService) Create(context.Context, r *apiv1.CreateRequest) (*apiv1.CreateResponse, error) {
-	return nil
+func (s *RunService) Create(ctx context.Context, r *apiv1.CreateRequest) (*apiv1.CreateResponse, error) {
+	return nil, nil
 }
-func (s *RunService) List(context.Context, r *apiv1.ListRequest) (*apiv1.ListResponse, error) {
-	return nil
+func (s *RunService) List(ctx context.Context, r *apiv1.ListRequest) (*apiv1.ListResponse, error) {
+	return nil, nil
 }
